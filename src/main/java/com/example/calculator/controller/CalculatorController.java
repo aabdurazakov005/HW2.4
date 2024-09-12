411f2441623a54d33e1c65671ff184a57b5f6a3f
aabdurazakov005 <aabdurazakov005@yandex.ru>
2024-09-12 15:45:44 +0300

    Домашнее доделанное задание 2.4

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -1,15 +1,43 @@
 package com.example.calculator.controller;
 
+import com.example.calculator.service.CalculatorService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
 
+    private final CalculatorService service;
+
+    public CalculatorController(CalculatorService service) {
+        this.service = service;
+    }
+
     @GetMapping
     public String hello(){
         return "Добро пожаловать в калькулятор";
+    }
+
+    @GetMapping("/plus")
+    public String plus(@RequestParam Integer num1, Integer num2) {
+        return num1 + "+" + num2 + "=" + service.plus(num1, num2);
+    }
+
+    @GetMapping("/minus")
+    public String minus(@RequestParam Integer num1, Integer num2) {
+        return num1 + "-" + num2 + "=" + service.minus(num1, num2);
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam Integer num1, Integer num2) {
+        return num1 + "/" + num2 + "=" + service.divide(num1, num2);
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam Integer num1, Integer num2) {
+        return num1 + "*" + num2 + "=" + service.multiply(num1, num2);
     }
 }
