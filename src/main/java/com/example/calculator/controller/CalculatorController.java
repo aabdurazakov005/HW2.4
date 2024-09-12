62f2b8991f6c69bf4349c1f7296fcb5abf645d6f
aabdurazakov005 <aabdurazakov005@yandex.ru>
2024-09-12 16:16:36 +0300

    Домашнее доделанное задание 2.4

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -22,22 +22,34 @@ public class CalculatorController {
     }
 
     @GetMapping("/plus")
-    public String plus(@RequestParam Integer num1, Integer num2) {
+    public String plus(@RequestParam(required = false) Integer num1,@RequestParam(required = false) Integer num2) {
+        if (num1 == null || num2 == null){
+            return "Ошибка";
+        }
         return num1 + "+" + num2 + "=" + service.plus(num1, num2);
     }
 
     @GetMapping("/minus")
-    public String minus(@RequestParam Integer num1, Integer num2) {
+    public String minus(@RequestParam(required = false) Integer num1,@RequestParam(required = false) Integer num2) {
+        if (num1 == null || num2 == null){
+            return "Ошибка";
+        }
         return num1 + "-" + num2 + "=" + service.minus(num1, num2);
     }
 
     @GetMapping("/divide")
-    public String divide(@RequestParam Integer num1, Integer num2) {
+    public String divide(@RequestParam(required = false)Integer num1,@RequestParam(required = false) Integer num2) {
+        if (num1 == null || num2 == null){
+            return "Ошибка";
+        }
         return num1 + "/" + num2 + "=" + service.divide(num1, num2);
     }
 
     @GetMapping("/multiply")
-    public String multiply(@RequestParam Integer num1, Integer num2) {
+    public String multiply(@RequestParam(required = false) Integer num1,@RequestParam(required = false) Integer num2) {
+        if (num1 == null || num2 == null){
+            return "Ошибка";
+        }
         return num1 + "*" + num2 + "=" + service.multiply(num1, num2);
     }
 }
